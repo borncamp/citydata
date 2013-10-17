298159587e6775e8a6fb14a780518340d3b05032
borncamp <borncamp@debian>
2013-10-17 01:33:25 -0400

    allrealprop gets all props for street

---
+++ allrealprop.rb
@@ -0,0 +1,70 @@
+require 'rubygems'
+require 'mechanize'
+
+agent=Mechanize.new
+page=agent.get "https://paytax.erie.gov/webprop/index.asp"
+@search_form=page.form_with :action=>"property_info_results.asp"
+
+
+def get_details number,street
+  @search_form.txtnum=number
+  @search_form.txtstreet=street
+  @search_form.Juris="1402"
+  result=@search_form.submit
+  details=result.link.click.content
+end
+
+def get_owner details
+  noko=Nokogiri::HTML(details)
+  table=noko.xpath "/html/body/div[@id='container']/div[@id='center_column_wide']/table[@id='generic_site_table']"
+  
+  ownername= table.xpath('./tr[2]/td[2]').text
+  street = table.xpath('./tr[6]/td[2]').text
+  citystate = table.xpath('./tr[7]/td[2]').text
+  zip = table.xpath('./tr[8]/td[2]').text
+
+  info={'ownername' => ownername,
+    'street' => street,
+    'city/state' => citystate,
+    'zip' => zip,
+    }
+end
+
+def get_houses_on_street street
+  @search_form.txtstreet=street
+  @search_form.Juris="1402"
+  result=@search_form.submit
+  prior_group=result
+  next_group=result.link_with(:href=>'property_info_results_next.asp').click
+  owners=[]
+  
+  while next_group.content != prior_group.content do
+    prior_group.links_with(:text=>'Details').each do |link|
+      owner=get_owner link.click.content
+      owners.push owner
+      puts owner
+    end
+    prior_group=next_group
+    next_group=result.link_with(:href=>'property_info_results_next.asp').click
+  end
+  next_group.links_with(:text=>'Details').each do |link|
+    owner=get_owner link.click.content
+    owners.push owner
+    puts owner
+  end
+  
+  return owners
+  
+end
+
+get_houses_on_street 'normal'
+#File.open(ARGV[0]).each_line do |line|
+# vars=line.split ','
+# number=vars[0]
+# street=vars[1]
+# details=get_details number,street
+# owner=get_owner details
+# puts number+" "+street+":"+owner.to_s
+# sleep 0.01
+#end 
+
