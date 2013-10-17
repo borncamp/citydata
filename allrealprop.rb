fd54ea765e918b97d9435e6751066ca9bb120c70
borncamp <borncamp@debian>
2013-10-17 02:57:56 -0400

    allrealprop all values for property

--- allrealprop.rb
+++ allrealprop.rb
@@ -17,17 +17,45 @@ end
 def get_owner details
   noko=Nokogiri::HTML(details)
   table=noko.xpath "/html/body/div[@id='container']/div[@id='center_column_wide']/table[@id='generic_site_table']"
-  
-  ownername= table.xpath('./tr[2]/td[2]').text
-  street = table.xpath('./tr[6]/td[2]').text
-  citystate = table.xpath('./tr[7]/td[2]').text
-  zip = table.xpath('./tr[8]/td[2]').text
+ 
+  #shit about house
+  info={
+    "parcelstatus" => table.xpath('./tr[1]/td[1]').text,
+    "sbl" => table.xpath('./tr[2]/td[1]').text,
+    "propertylocation" => table.xpath('./tr[3]/td[1]').text,
+    "propertyclass" => table.xpath('./tr[4]/td[1]').text,
+    "assessment" => table.xpath('./tr[5]/td[1]').text,
+    "taxable" => table.xpath('./tr[6]/td[1]').text,
+    "desc1" => table.xpath('./tr[7]/td[1]').text,
+    "desc2" => table.xpath('./tr[8]/td[1]').text,
+    "deedbook" => table.xpath('./tr[9]/td[1]').text,
+    "frontage" => table.xpath('./tr[10]/td[1]').text,
+    "yearbuilt" => table.xpath('./tr[11]/td[1]').text,
+    "beds" => table.xpath('./tr[12]/td[1]').text,
+    "fireplace" => table.xpath('./tr[13]/td[1]').text,
 
-  info={'ownername' => ownername,
-    'street' => street,
-    'city/state' => citystate,
-    'zip' => zip,
-    }
+    #about the owner ,
+    "ownername" => table.xpath('./tr[2]/td[2]').text,
+    "mailingaddress" => table.xpath('./tr[3]/td[2]').text,
+    "line2" => table.xpath('./tr[4]/td[2]').text,
+    "line3" => table.xpath('./tr[5]/td[2]').text,
+    "street" => table.xpath('./tr[6]/td[2]').text,
+    "citystate" => table.xpath('./tr[7]/td[2]').text,
+    "zip" => table.xpath('./tr[8]/td[2]').text,
+
+    #shit about house,
+    "deedpage" => table.xpath('./tr[9]/td[2]').text,
+    "depth" => table.xpath('./tr[10]/td[2]').text,
+    "squareft" => table.xpath('./tr[11]/td[2]').text,
+    "baths" => table.xpath('./tr[12]/td[2]').text,
+    "school" => table.xpath('./tr[13]/td[2]').text
+  }
+
+#  info={'ownername' => ownername,
+#    'street' => street,
+#    'city/state' => citystate,
+#    'zip' => zip,
+#    }
 end
 
 def get_houses_on_street street
@@ -54,7 +82,6 @@ def get_houses_on_street street
   end
   
   return owners
-  
 end
 
 File.open(ARGV[0]).each_line do |line|
