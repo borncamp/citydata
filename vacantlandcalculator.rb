6ada754386cc3676d512c192ec42c92cd8c43ad7
Your Name <you@example.com>
2013-12-12 21:56:25 +0000

    added commerical calculation to vacant calc

--- vacantlandcalculator.rb
+++ vacantlandcalculator.rb
@@ -12,4 +12,13 @@ readcoll.find('propertyclass'=> '311 RES VAC LAND').each do |row|
   value+=area
 end
 
-puts value
+puts "Residential sq ft: "+value.to_s
+
+value=0
+readcoll.find('propertyclass'=> '330 VACANT COMM').each do |row|
+  area=0
+  area=row['depth'].to_f*row['frontage'].to_f
+  value+=area
+end
+
+puts "Commerical sq ft: "+value.to_s
